29ef17dc11f8e15446f94d7f1265200ea4ceb628
thesambassador <thesambassador@gmail.com>
2014-05-22 22:59:51 -0700

    Character run cycle, aiming animation, and scripts to manage both

---
+++ Assets/Modules/Scripts/GameScripts/Player/gunLook.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using SmoothMoves;
+
+public class gunLook : MonoBehaviour {
+
+    public BoneAnimation boneAnimation;
+    public AnimationStateSM animationState;
+
+    private Transform _armTransform;
+    private Vector3 _armRotation;
+    private Vector3 _mouseOffset;
+    private Vector3 _transformScreenPosition;
+
+	// Use this for initialization
+	void Start () {
+        _armTransform = boneAnimation.GetBoneTransform("arms");
+
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        SetAimFrame();
+
+        
+
+    }
+
+	void SetAimFrame () {
+
+        //get the angle between the mouse and the player's gun
+        //0 = pointing straight to the right, 180 = pointing straight to the left
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 playerScreenPos = Camera.main.WorldToScreenPoint(_armTransform.position);
+
+        _mouseOffset = (mousePos - playerScreenPos);
+
+        float aimZ = Mathf.Atan2(_mouseOffset.y, _mouseOffset.x) * Mathf.Rad2Deg;
+        aimZ -= 90;
+
+        aimZ = ConvertTo180Scale(aimZ);
+
+        //flip the character if your mouse is to the right of the character, and we have to change the angle to have aiming straight upwards still equal 0
+        Vector2 newScale = new Vector2(-1, 1);
+
+        if (mousePos.x < playerScreenPos.x)
+        {
+            newScale.x = 1;
+            
+        }
+        else
+        {
+            newScale.x = -1;
+            aimZ -= 360;
+            aimZ = Mathf.Abs(aimZ);
+
+        }
+
+        //Debug.Log(aimZ);
+
+        boneAnimation["aim"].frame = aimZ;
+
+        transform.localScale = newScale;
+
+	}
+
+    float ConvertTo180Scale(float rawAngle)
+    {
+        float returned = rawAngle;
+
+        while (returned < 0)
+        {
+            returned += 360;
+        }
+
+        return returned;
+    }
+}
---
+++ Assets/Modules/Scripts/GenericScripts/CustomActions/Player/PlayerActions.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using HutongGames.PlayMaker;
+
+namespace HutongGames.Playmaker.Actions
+{
+    
+    [ActionCategory("BreachActions")]
+    [Tooltip("Figures whether the player is facing left or right and running left or right, then sets a variable to either -1 or 1")]
+    
+    public class SetPlayerAnimationSpeed :  FsmStateAction{
+
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Store running direction here, 1 for moving forward, -1 for moving backwards")]
+        public FsmFloat runningDir;
+
+	    // Use this for initialization
+	    public void Start () {
+	        
+	    }
+	
+	    // Update is called once per frame
+        public override void OnUpdate()
+        {
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 playerScreenPos = Camera.main.WorldToScreenPoint(this.Owner.transform.position);
+
+            float mouseX = mousePos.x;
+            float playerX = playerScreenPos.x;
+
+            bool keyRight = Input.GetKey(KeyCode.RightArrow);
+            bool keyLeft = Input.GetKey(KeyCode.LeftArrow);
+
+            bool movingTowardsMouse = (keyRight && mouseX > playerX) || (keyLeft && mouseX < playerX);
+
+            runningDir.Value = -1;
+
+            if (movingTowardsMouse)
+                runningDir.Value = 1;
+
+            Debug.Log("Test");
+
+	    }
+    }
+
+}
